df0e3e60aee4d51559609bbf5595b1f1ac3ea50f
Kayla Tokash <ktokash@demonfox01>
2020-01-21 04:42:51 +0000

    Updated build files

---
+++ CMakeFiles/interview.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/interview.dir/main.c.o"
+  "interview.pdb"
+  "interview"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/interview.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
